10c8b5a0b7ace2ec4ce0d9be870794c44d67cf77
matteov <matteo.volpi@epitech.eu>
2022-07-09 01:15:44 +0200

    add a using statement to prevent cluttered class definition

--- LuaFunction.hpp
+++ LuaFunction.hpp
@@ -5,16 +5,21 @@
 #include "NonVoidFunction.hpp"
 #include "VoidFunction.hpp"
 
+template< class T, class ...Args >
+using DerivingFunction = typename std::conditional<
+    std::is_void<T>::value,
+    VoidFunction< void(Args...) >,
+    NonVoidFunction< T(Args...) >
+>::type;
+
 template< class >
 class LuaFunction;
 
 template< class T, class ...Args >
-class LuaFunction < T(Args...) > : public std::conditional<
-    std::is_void<T>::value,
-    VoidFunction< void(Args...) >,
-    NonVoidFunction< T(Args...) > >::type
+class LuaFunction < T(Args...) > : public DerivingFunction<T, Args...>
 {
 public:
-    LuaFunction(Lua &l, const std::string &fname) : std::conditional< std::is_void<T>::value, VoidFunction< void(Args...) >, NonVoidFunction< T(Args...) > >::type(l, fname)
+    LuaFunction(Lua &l, const std::string &fname) :
+        DerivingFunction<T, Args...>(l, fname)
     {}
 };
